e2230cb1a6f1eb56422e77a85a27be2be4d5f005
NP-Code99 <65794460+NP-Code99@users.noreply.github.com>
2021-03-10 20:33:17 -0500

    Update tic-tac-toe.py

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -38,28 +38,53 @@ def settings_button(x, y, radius):
     pygame.draw.circle(screen, purpleish, (x, y), radius) # 27, 305, 30
     screen.blit(settings_button_text, (x - 25, y - 10))
 
+# Help Screen Text
+def instructions(y): # Continue to format the paragraph 
+    regular = pygame.font.Font(r'C:\WINDOWS\Fonts\TIMES.TTF', 14)
+    help_screen_txt = '''Tic-tac-toe, noughts and crosses, or Xs and Os/“X’y O’sies”, |is a paper-and-pencil game for two players, |X and O, who take turns marking the |spaces in a 3×3 grid. |The player who succeeds in placing |three of their marks in a |diagonal, horizontal, or vertical row is the winner.'''
+    for item in help_screen_txt.split('|'):
+        y += 20
+        screen.blit(regular.render(item, True, lime), (10, y))
+
 # Board
 board = pygame.image.load(r'C:\Users\Admin\OneDrive\Desktop\All Python Projects\Tic-tac-toe\All Images\ultimate-tic-tac-toe12-01.png')
 
 # Marks
 x_mark = pygame.image.load(r'C:\Users\Admin\OneDrive\Desktop\All Python Projects\Tic-tac-toe\All Images\close.png')
 
+dodger_blue = (0, 0, 205)
+white = (255, 255, 255)
+
+# Tic_tac_toe Screen
 def tic_tac_toe():
-    running = True
-    while running: 
-        screen.fill((255, 255, 255))
+    game_running = True
+    while game_running: 
+        screen.fill(white)
         screen.blit(board, (0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                game_running = False
         pygame.display.update()
 
-def main_menu(): # Create a Main Menu
-    running = True
+
+# Help Screen
+def help_screen():
+    help_running = True
+    while help_running: 
+        screen.fill(dodger_blue)
+        instructions(100)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                help_running = False
+        pygame.display.update()
+
+# Main Menu Screen
+def main_menu():
+    main_running = True
     button_y = 120
-    while running:
+    while main_running:
         # Background Color
-        screen.fill((0,0,205))
+        screen.fill((0, 0, 205))
         # Title
         screen.blit(title, (73, 20))
         # Rectangle Buttons
@@ -67,11 +92,15 @@ def main_menu(): # Create a Main Menu
         help_button(69, button_y + 70, 200, 50)
         # Circle Button
         settings_button(37, 305, 30)
+        # Events / Actions
+        mouse_pos_x, mouse_pos_y = pygame.mouse.get_pos()
         for event in pygame.event.get():
             if event.type == pygame.QUIT: 
-                running = False
+                main_running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                print(event.pos) # Don't forget to change this
+                if mouse_pos_x >= 69 and mouse_pos_x <= 270 and mouse_pos_y >= 189 and mouse_pos_y <= 239:
+                    main_running = False
+                    help_screen()
         pygame.display.update()
 
 main_menu()
