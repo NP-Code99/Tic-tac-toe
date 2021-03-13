eb1cbefaa258879e8289f4d4b19763c8796cfb16
NP-Code99 <65794460+NP-Code99@users.noreply.github.com>
2021-03-13 18:38:33 -0500

    Update tic-tac-toe.py

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -23,28 +23,30 @@ pygame.mixer.music.play(-1)
 # Buttons
 button_length = 200
 button_width = 50
-def game_button(x, y, length, width): 
+def game_button(x, y, length, width, color): 
     purpleish = (102, 102, 255)
     game_button = (x, y, length, width) # 70, 60, 200, 50
     game_button_text = cartoony_font.render('Play Game', True, lime)
-    pygame.draw.rect(screen, purpleish, game_button)
+    pygame.draw.rect(screen, color, game_button)
     screen.blit(game_button_text, (x + 38, y + 5))
 
-def help_button(x, y, length, width):
+def help_button(x, y, length, width, color):
+    purpleish1 = (102, 102, 255)
     help_button = (x, y, length, width) 
     help_button_text = cartoony_font.render('Help', True, lime)
-    pygame.draw.rect(screen, purpleish, help_button)
+    pygame.draw.rect(screen, color, help_button)
     screen.blit(help_button_text, (x + 69, y + 5))
 
-def settings_button(x, y, radius):
+def settings_button(x, y, radius, color):
     settings_button_text = settings_button_font.render('Settings', True, lime)
-    pygame.draw.circle(screen, purpleish, (x, y), radius) # 27, 305, 30
+    pygame.draw.circle(screen, color, (x, y), radius) # 27, 305, 30
     screen.blit(settings_button_text, (x - 25, y - 10))
 
-def back_button(x, y, length, width):
+
+def back_button(x, y, length, width, color):
     back_button = (x, y, length, width) 
     back_button_text = cartoony_font.render('Back', True, lime)
-    pygame.draw.rect(screen, purpleish, back_button)
+    pygame.draw.rect(screen, color, back_button)
     screen.blit(back_button_text, (x + 18, y + 6))
 
 # Instructions Text
@@ -75,44 +77,64 @@ def tic_tac_toe():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                game_running = False
+                main_menu()
         pygame.display.update()
 
 # Help Screen
 def help_screen():
     help_running = True
+    back_button_changeable_color = (102, 102, 255)
     while help_running: 
         screen.fill(dodger_blue)
         screen.blit(youtube_star_title.render('Instructions', True, lime), (74, 20))
         instructions_text(70)
-        back_button(10, 278, 100, 50)
+        back_button(10, 278, 100, 50, back_button_changeable_color)
         mouse_pos_x, mouse_pos_y = pygame.mouse.get_pos()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 help_running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                help_running = False
                 if mouse_pos_x >= 10 and mouse_pos_x <= 109 and mouse_pos_y >= 277 and mouse_pos_y <= 328:
+                    help_running = False
                     main_menu()
+        # Back Button Color Changing
+        if mouse_pos_x >= 10 and mouse_pos_x <= 109 and mouse_pos_y >= 277 and mouse_pos_y <= 328:
+            back_button_changeable_color =  (65, 105, 225)
+        else: 
+            back_button_changeable_color = (102, 102, 255)
         pygame.display.update()
 
 # Settings Screen
 def settings_screen():
     settings_running = True
+    back_button_changeable_color = (102, 102, 255)
     while settings_running:
         screen.fill(dodger_blue)
-        back_button(10, 278, 100, 50)
+        screen.blit(youtube_star_title.render('Settings', True, lime), (106, 20))
+        screen.blit(pygame.font.SysFont('youtube star', 22).render('Theme', True, lime), (80,100))
+        back_button(10, 278, 100, 50, back_button_changeable_color)
         mouse_pos_x, mouse_pos_y = pygame.mouse.get_pos()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 settings_running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                settings_running = False
                 if mouse_pos_x >= 10 and mouse_pos_x <= 109 and mouse_pos_y >= 277 and mouse_pos_y <= 328:
+                    settings_running = False
                     main_menu()
+        # Back Button Color Changing
+        if mouse_pos_x >= 10 and mouse_pos_x <= 109 and mouse_pos_y >= 277 and mouse_pos_y <= 328:
+            back_button_changeable_color = (65, 105, 225)
+        else: 
+            back_button_changeable_color = (102, 102, 255)
         pygame.display.update()
 
 # Main Menu Screen
 def main_menu():
+    game_button_changeable_color = (102, 102, 255)
+    help_button_changeable_color = (102, 102, 255)
+    settings_button_changeable_color = (102, 102, 255)
     main_running = True
     button_y = 120
     cursor = pygame.cursors.compile(pygame.cursors.textmarker_strings)
@@ -122,22 +144,40 @@ def main_menu():
         # Title
         screen.blit(title, (73, 20))
         # Rectangle Buttons
-        game_button(69, button_y, button_length, button_width)
-        help_button(69, button_y + 70, button_length, button_width)
+        game_button(69, button_y, button_length, button_width, game_button_changeable_color)
+        help_button(69, button_y + 70, button_length, button_width, help_button_changeable_color)
         # Circle Button
-        settings_button(37, 305, 30)
+        settings_button(37, 305, 30, settings_button_changeable_color)
         # Events / Actions
         mouse_pos_x, mouse_pos_y = pygame.mouse.get_pos()
         for event in pygame.event.get():
             if event.type == pygame.QUIT: 
                 main_running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
+                if mouse_pos_x >= 69 and mouse_pos_x <= 270 and mouse_pos_y >= 119 and mouse_pos_y <= 169:
+                    main_running = False
+                    tic_tac_toe()
                 if mouse_pos_x >= 69 and mouse_pos_x <= 270 and mouse_pos_y >= 189 and mouse_pos_y <= 239:
                     main_running = False
                     help_screen()
                 if mouse_pos_x >= 7 and mouse_pos_x <= 66 and mouse_pos_y >= 275 and mouse_pos_y <= 335:
                     main_running = False
                     settings_screen()
+        # Game Button Color Changing
+        if mouse_pos_x >= 69 and mouse_pos_x <= 270 and mouse_pos_y >= 119 and mouse_pos_y <= 169:
+            game_button_changeable_color = (65, 105, 225)
+        else:
+            game_button_changeable_color = (102, 102, 255)
+        # Help Button Color Changing
+        if mouse_pos_x >= 69 and mouse_pos_x <= 270 and mouse_pos_y >= 189 and mouse_pos_y <= 239:
+            help_button_changeable_color = (65, 105, 225)
+        else:
+            help_button_changeable_color = (102, 102, 255)
+        # Settings Button Color Changing
+        if mouse_pos_x >= 7 and mouse_pos_x <= 66 and mouse_pos_y >= 275 and mouse_pos_y <= 335:
+            settings_button_changeable_color = (65, 105, 225)
+        else:
+            settings_button_changeable_color = (102, 102, 255)
         pygame.display.update()
 
 main_menu()
